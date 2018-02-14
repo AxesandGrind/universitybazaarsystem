9f02a7b4e3f7c337430422d474963dda242d4682
Sunny Shah <shah.sk0901@gmail.com>
2018-02-14 15:10:13 -0600

    adding src files

---
+++ app/src/main/java/com/advse/universitybazaar/register/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.advse.universitybazaar.register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
