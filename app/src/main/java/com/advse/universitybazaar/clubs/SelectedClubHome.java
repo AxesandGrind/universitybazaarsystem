fa5db9ab5d42b721b0eabe03bf88787b339aa10c
Sunny Shah <shah.sk0901@gmail.com>
2018-02-28 15:22:03 -0600

    Selected club activity created alongwith minor UI fixes

---
+++ app/src/main/java/com/advse/universitybazaar/clubs/SelectedClubHome.java
@@ -0,0 +1,13 @@
+package com.advse.universitybazaar.clubs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectedClubHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_club_home);
+    }
+}
