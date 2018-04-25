4b0a1adeb4e9e9f017e8bab51ea2fd18dad519b1
Sunny Shah <shah.sk0901@gmail.com>
2018-04-25 03:09:28 -0500

    base activity added for navigation view

--- app/src/main/java/com/advse/universitybazaar/bean/BaseActivity.java
+++ app/src/main/java/com/advse/universitybazaar/bean/BaseActivity.java
@@ -1,67 +1,84 @@
 package com.advse.universitybazaar.bean;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
-import android.view.Menu;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.advse.universitybazaar.R;
+import com.advse.universitybazaar.clubs.ClubHome;
+import com.advse.universitybazaar.messages.MessageHome;
+import com.advse.universitybazaar.posts.PostHome;
+import com.advse.universitybazaar.trade.TradeActivity;
+import com.advse.universitybazaar.register.UserHome;
 
+import java.lang.reflect.Field;
+
+// Example can be found at link below
+// https://blog.fossasia.org/using-activities-with-bottom-navigation-view-in-phimpme-android/
 public abstract class BaseActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    protected BottomNavigationView navigationView;
+    protected BottomNavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(getContentViewId());
-
-        navigationView = (BottomNavigationView) findViewById(R.id.navigation);
-        navigationView.setOnNavigationItemSelectedListener(this);
+        setContentView(getContentView());
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        updateNavigationBarState();
+        updateNavigation();
     }
 
     // Remove inter-activity transition to avoid screen tossing on tapping bottom navigation items
     @Override
-    public void onPause() {
+    protected void onPause() {
         super.onPause();
         overridePendingTransition(0, 0);
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        navigationView.postDelayed(() -> {
+    public boolean onNavigationItemSelected(@NonNull final MenuItem item) {
+        navigation.postDelayed(() -> {
             int itemId = item.getItemId();
-            if (itemId == R.id.navigation_home) {
-                startActivity(new Intent(this, HomeActivity.class));
-            } else if (itemId == R.id.navigation_dashboard) {
-                startActivity(new Intent(this, DashboardActivity.class));
-            } else if (itemId == R.id.navigation_notifications) {
-                startActivity(new Intent(this, NotificationsActivity.class));
+
+            if(itemId == R.id.navigation_news) {
+                startActivity(new Intent(this,PostHome.class));
+            } else if(itemId == R.id.navigation_clubs) {
+                startActivity(new Intent(this,ClubHome.class));
+            } else if(itemId == R.id.navigation_market) {
+                startActivity(new Intent(this,TradeActivity.class));
+            } else if(itemId == R.id.navigation_messages) {
+                startActivity(new Intent(this, MessageHome.class));
+            } else if(itemId == R.id.navigation_settings) {
+                startActivity(new Intent(this,UserHome.class));
             }
             finish();
-        }, 300);
+        },300);
         return true;
     }
 
-    private void updateNavigationBarState(){
-        int actionId = getNavigationMenuItemId();
-        selectBottomNavigationBarItem(actionId);
+    private void updateNavigation() {
+        int id = getNavigationId();
+        setNavigationItem(id);
     }
 
-    void selectBottomNavigationBarItem(int itemId) {
-        MenuItem item = navigationView.getMenu().findItem(itemId);
+    private void setNavigationItem(int itemId) {
+        MenuItem item = navigation.getMenu().findItem(itemId);
         item.setChecked(true);
     }
 
-    abstract int getContentViewId();
-
-    abstract int getNavigationMenuItemId();
-
+    public abstract int getContentView();
+    public abstract int getNavigationId();
 }
